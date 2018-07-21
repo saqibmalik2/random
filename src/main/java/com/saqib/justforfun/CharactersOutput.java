cc7436c7188326533b75cafc833904ebed928c35
Saqib Malik <saqib.malik2@gmail.com>
2018-07-21 10:45:20 +0100

    Clean up

--- src/main/java/com/saqib/justforfun/CharactersOutput.java
+++
@@ -1,9 +0,0 @@
-package com.saqib.justforfun;
-
-public class CharactersOutput {
-	
-	public static void main (String...args) {
-		"abracadabra".chars().distinct().peek(ch -> System.out.printf("%c", ch)).sorted().forEach(System.out::println);;
-	}
-
-}
