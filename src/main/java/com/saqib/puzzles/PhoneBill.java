19c3c62dd8d387956b8fa760b49a3cf1a422a53c
Saqib Malik <saqib.malik2@gmail.com>
2018-07-21 10:18:07 +0100

    Description added

--- src/main/java/com/saqib/puzzles/PhoneBill.java
+++ src/main/java/com/saqib/puzzles/PhoneBill.java
@@ -7,6 +7,15 @@ import java.util.Map;
  * 
  * Codility test. A variant of this question is one of the stock questions in the Codility test suite.
  * 
+ *  Given a phone calls log in the form of a string with each record separated by \n (ascii code 10) calculate the total phone bill.
+ *  Each record is correctly formed as "HH:MM:SS,nnn-nnn-nnn" e.g. "00:01:07,400-234-090" refers to a phone call to phone number 
+ *  400-234-090 and lasted a duration of 1 minute and 7 seconds.
+ *  Rules for calculating charges:
+ *		   (i) For a phone call less than 5 minutes long charge 3 cents per second
+ *		   (ii) For a phone call 5 minutes and longer charge 150 cents per started minute, so for a call of 5 minutes 2 seconds would be
+ *		        charged as a 6 minute call i.e. 6 * 150 = 900 cents
+ *		   (iii) All calls to the phone number with the longest total call duration are free. In the event that two phone numbers have the same total call duration use the one
+ * 
  * @author Saqib Malik (2018)
  *
  */
