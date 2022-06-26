894c64b25dcb3b726900d9a655016e6b2d54aa67
munda <munda@SaqibLaptop.Home>
2022-06-26 11:38:11 +0100

    Added Chess Player (Hacker Rank)

---
+++ src/main/java/com/saqib/puzzles/ChessPlayer.java
@@ -0,0 +1,45 @@
+package com.saqib.puzzles;
+
+import java.util.*;
+
+//Write your Checker class here
+class Checker implements Comparator<Player> {
+ 
+	 public int compare(Player a, Player b){
+	     if (a.score > b.score) return -1;
+	     else if (a.score < b.score) return 1;
+	     else return (a.name.compareTo(b.name));
+	 }
+ 
+}
+
+class Player{
+	 String name;
+	 int score;
+	 
+	 Player(String name, int score){
+	     this.name = name;
+	     this.score = score;
+	 }
+}
+
+public class ChessPlayer {
+
+	 public static void main(String[] args) {
+	     Scanner scan = new Scanner(System.in);
+	     int n = scan.nextInt();
+	
+	     Player[] player = new Player[n];
+	     Checker checker = new Checker();
+	     
+	     for(int i = 0; i < n; i++){
+	         player[i] = new Player(scan.next(), scan.nextInt());
+	     }
+	     scan.close();
+	  
+	     Arrays.sort(player, checker);
+	     for(int i = 0; i < player.length; i++){
+	         System.out.printf("%s %s\n", player[i].name, player[i].score);
+	     }
+	 }
+}
