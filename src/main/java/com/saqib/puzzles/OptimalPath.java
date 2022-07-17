86f67cbf5c64634b5fa842808549959fbb06e599
munda <munda@SaqibLaptop.Home>
2022-07-17 11:30:35 +0100

    Added comment to explain Optimal Path

--- src/main/java/com/saqib/puzzles/OptimalPath.java
+++ src/main/java/com/saqib/puzzles/OptimalPath.java
@@ -56,7 +56,7 @@ public class OptimalPath {
 			//the starting total is the value of the first position (bottom left)
 			total = grid[grid.length - 1][0];
 			
-			//if we have only entry in the matrix then its value is the total
+			//if we have only entry in the matrix then its value is the total. this represents the base case for the recursion.
 			if (grid.length == 1 && grid[0].length == 1) {
 				return total;
 			}
@@ -69,21 +69,26 @@ public class OptimalPath {
 				return total += calculateOptimalPath(newGrid);
 			}
 			
-			//if the length of the top row is one then we can total up the one value in each of the arrays 
+			//if the length of the top row is one (and as all arrays are the same length) then we can total up the one value in each of the arrays 
 			if (grid[0].length == 1) {
 				int [][] newGrid = new int[1][1];
 				newGrid[0][0] = grid[0][0];
 				return total += calculateOptimalPath(newGrid);
 			}
+			//we have two choices: move up or move along
 			else {
+				//the up path choice involves creating a new grid with the left most corner being the position directly above the current position
 				int[][] upPathGrid = new int[grid.length - 1][];
 				for (int i = grid.length - 2; i >= 0; i--) {
 					upPathGrid[i] = Arrays.copyOfRange(grid[i], 0, grid[0].length);
 				}
+				//the along path choice involves creating a new grid with the left most corner being the position directly to the right of the current position
 				int [][] alongPathGrid = new int[grid.length][grid[0].length - 1];
 				for (int i = grid.length -1; i >= 0; i--) {
 					alongPathGrid[i] = Arrays.copyOfRange(grid[i], 1, grid[0].length);
 				}
+				// apply recursion: add the current total to the maximum value of either the up or along paths. this line is where the "magic" resides
+				// and to understand it properly I recommend watching a YouTube video on "back tracking" algorithms.
 				return total += Math.max(calculateOptimalPath(upPathGrid), calculateOptimalPath(alongPathGrid));
 			}
 	}
