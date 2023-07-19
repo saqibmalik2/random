5d162b8a79154e366bece3f65e97e94f0339e1b6
saqibmalik2 <saqib.malik2@gmail.com>
2023-07-19 14:22:07 +0100

    Explanation that we're using an integer number between 11 and 14 to represent the picture cards (Jack/Queen/King/Ace).

--- src/main/java/com/saqib/puzzles/poker/Card.java
+++ src/main/java/com/saqib/puzzles/poker/Card.java
@@ -2,6 +2,7 @@ package com.saqib.puzzles.poker;
 
 public class Card {
 	
+	// A card has a value of between 1 and 14 (Jack =11, Queen = 12, King = 13, Ace = 14) as well as a Suit (HEARTS, DIAMONDS, SPADES, CLUBS).
 	Integer value;
 	Suit suit;
 	
