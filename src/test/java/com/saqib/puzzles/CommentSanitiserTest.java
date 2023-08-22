31257daae926ea80f4e489d280705f211e6a2d17
saqibmalik2 <saqib.malik2@gmail.com>
2023-08-22 14:02:29 +0100

    Added some more unit tests.

--- src/test/java/com/saqib/puzzles/CommentSanitiserTest.java
+++ src/test/java/com/saqib/puzzles/CommentSanitiserTest.java
@@ -7,30 +7,40 @@ import org.junit.jupiter.api.Test;
 public class CommentSanitiserTest {
 	
 	String originalComment = "James was a crap builder and generally a cretin. Send him your complaints on james@jollycleverbuilders.com or (07965123456)";
-	String originalComment1 = "James was: an idiot,idler and generally a cretin. Send him your complaints on james@jollycleverbuilders.com or (07965123456)";
-	
+	String commaSeparatedAbusiveWords = "James was: an idiot,idler and generally a cretin. Send him your complaints on james@jollycleverbuilders.com or (07965123456)";
+	String moreThanTwoPhoneNumbers = "James was: an idiot,idler and generally a cretin. Send him your complaints on james@jollycleverbuilders.com or (07965123456) or (07958178325)";
+	String moreThanTwoEmailAddresses = "James was: an idiot,idler and generally a cretin. Send him your complaints on james@jollycleverbuilders.com or jimmy@cowboybuilders.com or (07965123456)";
 	
 	@Test
 	void sanitiseCommentTest() {
 		CommentSanitiser commentSanitiser = new CommentSanitiser();
 		String sanitisedComment = commentSanitiser.sanitiseComment(originalComment);
 		
-		System.out.println(sanitisedComment);
-		System.out.println("James was a **** builder and generally a ******. Send him your complaints on <email address> or (<phone number>)");
-		
 		assertEquals("James was a **** builder and generally a ******. Send him your complaints on <email address> or (<phone number>)", sanitisedComment);
 	}
 	
 	@Test
-	void sanitiseCommentTest1() {
+	void sanitiseCommentCommaSeparatedAbusiveTest() {
 		CommentSanitiser commentSanitiser = new CommentSanitiser();
-		String sanitisedComment = commentSanitiser.sanitiseComment(originalComment1);
-		
-		System.out.println(sanitisedComment);
-		System.out.println("James was: an *****,***** and generally a ******. Send him your complaints on <email address> or (<phone number>)");
+		String sanitisedComment = commentSanitiser.sanitiseComment(commaSeparatedAbusiveWords);
 		
 		assertEquals("James was: an *****,***** and generally a ******. Send him your complaints on <email address> or (<phone number>)", sanitisedComment);
 	}
 	
+	@Test
+	void sanitiseCommentPhoneNumbersTest() {
+		CommentSanitiser commentSanitiser = new CommentSanitiser();
+		String sanitisedComment = commentSanitiser.sanitiseComment(moreThanTwoPhoneNumbers);
+		
+		assertEquals("James was: an *****,***** and generally a ******. Send him your complaints on <email address> or (<phone number>) or (<phone number>)", sanitisedComment);
+	}
+	
+	@Test
+	void sanitiseCommentTwoEmailAddressesTest() {
+		CommentSanitiser commentSanitiser = new CommentSanitiser();
+		String sanitisedComment = commentSanitiser.sanitiseComment(moreThanTwoEmailAddresses);
+		
+		assertEquals("James was: an *****,***** and generally a ******. Send him your complaints on <email address> or <email address> or (<phone number>)", sanitisedComment);
+	}
 	
 }
