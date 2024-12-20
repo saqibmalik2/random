b8b2bddb4b359fdede7698cb3633c2f0b53f41f9
saqibmalik2 <saqib.malik2@gmail.com>
2024-12-20 23:09:11 +0000

    Leet code test. Greedy algorithm.

--- src/main/java/com/saqib/puzzles/MergeIntervals.java
+++ src/main/java/com/saqib/puzzles/MergeIntervals.java
@@ -6,7 +6,7 @@ import java.util.Stack;
 
 /*****
  
-Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, 
+Given an array of intervals where intervals[i] = [starting, ending], merge all overlapping intervals, 
 and return an array of the non-overlapping intervals that cover all the intervals in the input.
 
 Example 1:
